5a33479074c524c902ccdd9062490ef59d644a94
Reedo0910 <sea.sand@live.cn>
2024-12-21 18:49:06 -0500

    feat(settings): documentation link
    add help documentation link into settings

--- src/renderer/scripts/settings.js
+++ src/renderer/scripts/settings.js
@@ -1,5 +1,8 @@
 const windowTitle = document.getElementById('window-title');
 
+const helpMessageTitle = document.getElementById('help-message-title');
+const helpMessageText = document.getElementById('help-message-text');
+
 const providerSelect = document.getElementById('provider-select');
 const providerSelectLabel = document.getElementById('provider-select-label');
 const providerSelectNote = document.getElementById('provider-select-note');
@@ -53,6 +56,14 @@ const LLMList = window.electronAPI.getInitLLMList();
 const isMac = window.electronAPI.isMacOS();
 
 document.title = translations['setting_window_title'];
+
+helpMessageTitle.textContent = translations['help_message_title'];
+
+const helpMessageTextContentTemplate = translations['help_message_text'];
+const dynamicLink = `<a id="readme-link" href="${translations['readme_link']}" target="_blank"><span id="github-readme-link-text">${translations['readme_link_text']}</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"><path d="M6.22 8.72a.75.75 0 0 0 1.06 1.06l5.22-5.22v1.69a.75.75 0 0 0 1.5 0v-3.5a.75.75 0 0 0-.75-.75h-3.5a.75.75 0 0 0 0 1.5h1.69L6.22 8.72Z" /><path d="M3.5 6.75c0-.69.56-1.25 1.25-1.25H7A.75.75 0 0 0 7 4H4.75A2.75 2.75 0 0 0 2 6.75v4.5A2.75 2.75 0 0 0 4.75 14h4.5A2.75 2.75 0 0 0 12 11.25V9a.75.75 0 0 0-1.5 0v2.25c0 .69-.56 1.25-1.25 1.25h-4.5c-.69 0-1.25-.56-1.25-1.25v-4.5Z" /></svg></a>`;
+const helpMessageTextContent = helpMessageTextContentTemplate.replace('[[readme_link_text]]', dynamicLink);
+
+helpMessageText.innerHTML = helpMessageTextContent;
 
 providerSelectLabel.textContent = translations['provider_select_label'];
 providerSelectNote.textContent = translations['provider_select_note'];
